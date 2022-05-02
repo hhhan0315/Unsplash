f1af1968ccecff94a5e9ecf59d6bb1693436d183
hhhan0315 <hhan0315@naver.com>
2022-05-02 16:35:42 +0900

    Feat: 다크모드로 고정

--- Unsplash/Unsplash/Supporting Files/SceneDelegate.swift
+++ Unsplash/Unsplash/Supporting Files/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .dark
         window.rootViewController = MainTabController()
         window.makeKeyAndVisible()
         self.window = window
