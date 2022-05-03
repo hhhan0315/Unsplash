3e2536910a2ed93bae8c425eb25a240a31b9c8cf
hhhan0315 <hhan0315@naver.com>
2022-05-03 15:37:19 +0900

    Feat: Self-sizing cell https://medium.com/geekculture/swift-implement-self-sizing-uicollectionview-cells-in-6-line-of-code-or-less-bf4944d62f9

---
+++ Unsplash/Unsplash/TopicCell.swift
@@ -0,0 +1,45 @@
+//
+//  TopicCell.swift
+//  Unsplash
+//
+//  Created by rae on 2022/05/03.
+//
+
+import UIKit
+
+class TopicCell: UICollectionViewCell {
+    static let identifier: String = String(describing: TopicCell.self)
+    
+    let button: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension TopicCell {
+    func configure() {
+        self.configureUI()
+    }
+    
+    func configureUI() {
+        self.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8.0),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8.0),
+        ])
+    }
+}
