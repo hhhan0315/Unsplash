e339561d2f0befbc5b6ff737f0cc653998ea638b
hhhan0315 <hhan0315@naver.com>
2022-05-03 16:25:33 +0900

    Feat: Carousel CollectionView 구현 UICollectionViewDiffableDataSource 활용

--- Unsplash/Unsplash/HomeViewController.swift
+++ Unsplash/Unsplash/HomeViewController.swift
@@ -9,15 +9,63 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    enum Section {
+        case topics
+        case photos
+    }
+    
+    private let topicCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(TopicCell.self, forCellWithReuseIdentifier: TopicCell.identifier)
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+    
+    private var topicDataSource: UICollectionViewDiffableDataSource<Section, Topic>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureUI()
+        self.configure()
+    }
+}
+
+private extension HomeViewController {
+    func configure() {
+        self.configureUI()
+        self.configureTopicDataSource()
     }
     
-    private func configureUI() {
+    func configureUI() {
         navigationItem.title = "Unsplash"
         
-        view.backgroundColor = .black
+        view.addSubview(topicCollectionView)
+        
+        NSLayoutConstraint.activate([
+            topicCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topicCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            topicCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            topicCollectionView.heightAnchor.constraint(equalToConstant: 60.0),
+        ])
+    }
+    
+    func configureTopicDataSource() {
+        self.topicDataSource = UICollectionViewDiffableDataSource<Section, Topic>(collectionView: self.topicCollectionView, cellProvider: { collectionView, indexPath, topic in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopicCell.identifier, for: indexPath) as? TopicCell else {
+                return TopicCell()
+            }
+            cell.button.setTitle(topic.rawValue, for: .normal)
+            return cell
+        })
+        
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Topic>()
+        snapShot.appendSections([.topics])
+        snapShot.appendItems(Topic.allCases, toSection: .topics)
+        self.topicDataSource?.apply(snapShot)
     }
 }
