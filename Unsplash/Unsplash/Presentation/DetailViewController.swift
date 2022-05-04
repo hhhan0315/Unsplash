ed2d799672d632416e3541111a171eb4a8568394
hhhan0315 <hhan0315@naver.com>
2022-05-04 12:00:17 +0900

    Style: Presentation 폴더 생성

---
+++ Unsplash/Unsplash/Presentation/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Unsplash
+//
+//  Created by rae on 2022/05/03.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+    }
+}
