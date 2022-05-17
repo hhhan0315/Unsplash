c5af7042c1a0c135bcde03aa9b5422f0e4d46dab
hhhan0315 <hhan0315@naver.com>
2022-05-17 18:34:42 +0900

    [Feat] DefaultSearchPhotoUseCase, SearchPhotoUseCase 구현

---
+++ Unsplash/Unsplash/Domain/UseCase/Protocol/SearchPhotoUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  SearchPhotoUseCase.swift
+//  Unsplash
+//
+//  Created by rae on 2022/05/17.
+//
+
+import Foundation
+
+protocol SearchPhotoUseCase {
+    func fetch(query: String, page: Int, completion: @escaping (Result<[Photo], Error>) -> Void)
+}
