e59abe0968c2bfaa3c3cbdbd193c6d2ac348cbaa
hhhan0315 <hhan0315@naver.com>
2022-05-17 18:35:36 +0900

    [Feat] SearchResult Codable 모델 구현

--- Unsplash/Unsplash/Domain/Model/Photo.swift
+++ Unsplash/Unsplash/Domain/Model/Photo.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Photo: Codable, Hashable, Equatable {
     let id: String
-    let urls: URLs
+    let urls: Urls
     
     static func == (lhs: Photo, rhs: Photo) -> Bool {
         return lhs.id == rhs.id
@@ -20,7 +20,7 @@ struct Photo: Codable, Hashable, Equatable {
     }
 }
 
-struct URLs: Codable {
+struct Urls: Codable {
     let raw: String
     let full: String
     let regular: String
