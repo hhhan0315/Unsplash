5586011bc3fb300ef88cbbabb3e25d8d300ca717
hhhan0315 <hhan0315@naver.com>
2022-05-17 18:34:21 +0900

    [Feat] DefaultSearchPhotoRepository, SearchPhotoRepository 구현

---
+++ Unsplash/Unsplash/Data/Repository/DefaultSearchPhotoRepository.swift
@@ -0,0 +1,28 @@
+//
+//  DefaultSearchPhotoRepository.swift
+//  Unsplash
+//
+//  Created by rae on 2022/05/17.
+//
+
+import Foundation
+
+final class DefaultSearchPhotoRepository: SearchPhotoRepository {
+    private let networkService: NetworkService
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    func fetch(query: String, page: Int, completion: @escaping (Result<[Photo], Error>) -> Void) {
+        let request = SearchPhotoRequest(query: query, page: page)
+        self.networkService.request(request) { (result: Result<SearchResult, NetworkError>) in
+            switch result {
+            case .success(let searchResult):
+                completion(.success(searchResult.results))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
---
+++ Unsplash/Unsplash/Data/Repository/Protocol/SearchPhotoRepository.swift
@@ -0,0 +1,12 @@
+//
+//  SearchPhotoRepository.swift
+//  Unsplash
+//
+//  Created by rae on 2022/05/17.
+//
+
+import Foundation
+
+protocol SearchPhotoRepository {
+    func fetch(query: String, page: Int, completion: @escaping (Result<[Photo], Error>) -> Void)
+}
